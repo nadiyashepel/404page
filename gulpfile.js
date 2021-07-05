83416bc4f43e7b8c07d875fe80ac8c6efd896d1b
nadiyashepel <83579214+nadiyashepel@users.noreply.github.com>
2021-07-05 21:51:23 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const del = require('del');
+
+gulp.task('styles', () => {
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./app/css/'));
+});
+
+gulp.task('clean', () => {
+    return del([
+        './app/css/main.css',
+    ]);
+});
+
+gulp.task('default', gulp.series(['clean', 'styles']));
+
+gulp.task('watch', () => {
+    gulp.watch('./app/scss/**/*.scss', (done) => {
+        gulp.series(['clean', 'styles'])(done);
+    });
+});
